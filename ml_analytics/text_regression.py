fcc44c659eebc3f159ac886b68805cb0bc8f8db9
Bondarchuk Gleb <72991231+BondarchukGleb42@users.noreply.github.com>
2023-08-14 03:14:11 +0700

    Add files via upload

---
+++ ml_analytics/text_regression.py
@@ -0,0 +1,23 @@
+from catboost import CatBoostRegressor
+import pandas as pd
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_path', type=str, default='C:/Deep Learning/ml_analytics/data/catboost_text_regressor')
+parser.add_argument('--json_path', type=str, default='C:/Deep Learning/ml_analytics/resule_parsing.json')
+args = parser.parse_args()
+
+def main():
+    model = CatBoostRegressor()      # parameters not required.
+    model.load_model(args.model_path)
+
+    with open(args.json_path, encoding='utf-8') as f:
+        text = json.load(f)[0]['description']
+
+    inputs = pd.DataFrame({'descriptions':[text]})
+    predict = abs(round(model.predict(inputs)[0], 6))
+    print(predict)
+
+if __name__ == '__main__':
+    main()
